cf19f42945e1b2629e914244c0095bd9ce4622cf
Bence <andrasibence00@gmail.com>
2022-10-28 21:11:33 +0200

    style+added localStorage

--- Counter/script.js
+++ Counter/script.js
@@ -3,23 +3,30 @@ var plus=document.getElementById("plus");
 var resetbtn=document.getElementById("reset");
 var number=document.getElementById("number");
 
-let a = 0;
+//localStorage.getItem("counter");
+let counter =localStorage.getItem("counter") || 0;
+
+setTimeout(number.innerHTML=localStorage.getItem("counter")|| 0,0);
 
 minus.addEventListener("click", decrease);
 plus.addEventListener("click", increase);
 resetbtn.addEventListener("click", reset);
 
 function increase(){
-    a++;
-    number.innerHTML=a;
+    counter++;
+    localStorage.setItem("counter", counter);
+    number.innerHTML=counter;
 }
 
 function decrease(){
-    a--;
-    number.innerHTML=a;
+    counter--;
+    localStorage.setItem("counter", counter);
+    number.innerHTML=counter;
+    
 }
 
 function reset(){
-    a=0;
-    number.innerHTML=a;
+    counter=0;
+    number.innerHTML=counter;
+    localStorage.setItem("counter", counter);
 }
