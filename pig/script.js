90381c5396ac76a5e0a49c633435fe0f8a22f6f7
Bence <andrasibence00@gmail.com>
2022-12-07 16:58:21 +0100

    pig game

---
+++ pig/script.js
@@ -0,0 +1,14 @@
+var newGameBtn;
+var rollBtn;
+var holdBtn;
+var dice;
+
+newGameBtn=document.getElementById("new-game")
+rollBtn=document.getElementById("roll")
+holdBtn=document.getElementById("hold")
+dice=document.getElementById("dice")
+
+var scores=[0,0]
+var currentScore=0
+var acticePlayer=0
+var playing=true
