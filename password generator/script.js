dcb64852dbab9a07fb5069523fb09357c1f0fab1
Bence <andrasibence00@gmail.com>
2023-01-03 19:40:15 +0100

    idegesített egy enter

--- password generator/script.js
+++ password generator/script.js
@@ -8,7 +8,6 @@ let lowercaseCheckbox=document.getElementById("lowercase");
 let numberCheckbox=document.getElementById("number");
 let symbolCheckbox=document.getElementById("symbol");
 
-
 const uppercase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowercase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const number=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
